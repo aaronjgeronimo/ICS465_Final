6d452b97a6f4dc2f25acbca084a1fca383ce9279
kainyogi <kainyogi@hawaii.edu>
2020-05-08 15:25:15 -1000

    Revert "Final Commit"
    This reverts commit 68c2e7b22fb442183e1d12a2ede048eb23aae35a.

--- app/imports/ui/pages/Statistics.jsx
+++ app/imports/ui/pages/Statistics.jsx
@@ -1,36 +1,8 @@
 import React from 'react';
-import { Divider, Grid, Header, Loader, Container } from 'semantic-ui-react';
+import { Divider, Grid, Header, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-import { Mentors } from '/imports/api/mentor/mentor';
-import { Profile } from '/imports/api/profile/profile';
-import { Bar } from 'react-chartjs-2';
-
-const state = {
-    labels: ['STEM', 'Social Sciences', 'Humanities'],
-    datasets: [
-        {
-            label: 'Classes',
-            backgroundColor: 'rgba(75,192,192,1)',
-            borderColor: 'rgba(0,0,0,1)',
-            borderWidth: 2,
-            data: [10, 5, 15],
-        },
-    ],
-};
-const state2 = {
-    labels: ['STEM', 'Social Sciences', 'Humanities'],
-    datasets: [
-        {
-            label: 'Students',
-            backgroundColor: 'rgba(75,192,192,1)',
-            borderColor: 'rgba(0,0,0,1)',
-            borderWidth: 2,
-            data: [8, 0, 4],
-        },
-    ],
-};
 
 class Statistics extends React.Component {
 
@@ -41,76 +13,28 @@ class Statistics extends React.Component {
 
   renderPage() {
     return (
-        <div className="percent-padding-bottom margin-top">
-            <Container>
-                <div>
-                <Bar
-                    data={state}
-                    options={{
-                        title: {
-                            display: true,
-                            text: 'Subjects Per Departments',
-                            fontSize: 20,
-                        },
-                        legend: {
-                            display: true,
-                            position: 'right',
-                        },
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    suggestedMin: 0,
-                                    suggestedMax: 20,
-                                },
-                            }],
-                        },
-                    }}
-                />
-            </div>
-                <br/>
-                <div>
-                    <Bar
-                        data={state2}
-                        options={{
-                            title: {
-                                display: true,
-                                text: 'Students Per Department',
-                                fontSize: 20,
-                            },
-                            legend: {
-                                display: true,
-                                position: 'right',
-                            },
-                            scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        suggestedMin: 0,
-                                        suggestedMax: 20,
-                                    },
-                                }],
-                            },
-                        }}
-                    />
-                </div>
-            </Container>
-        </div>
+        <Grid container centered>
+          <Grid.Column>
+            <Divider hidden />
+            <Header as="h2" textAlign="center">Statistics</Header>
+          </Grid.Column>
+        </Grid>
     );
   }
 }
 
 /** Require an array of Stuff documents in the props. */
 Statistics.propTypes = {
-    mentors: PropTypes.array.isRequired,
-    student: PropTypes.array.isRequired,
-    ready: PropTypes.bool.isRequired,
+  // stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-    const subscription = Meteor.subscribe('Mentor');
-    const subscription2 = Meteor.subscribe('ProfileLanding');
-    return {
-        mentors: Mentors.find({}).fetch(),
-        student: Profile.find({}).fetch(),
-        ready: subscription.ready() && subscription2.ready(),
-    };
+  // Get access to Stuff documents.
+    const subscription = Meteor.subscribe('Statistics');
+  return {
+    // stuffs: Stuffs.find({}).fetch(), Nothing to fetch yet
+    ready: subscription.ready(),
+  };
 })(Statistics);
